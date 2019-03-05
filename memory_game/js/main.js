1581c0bf465a4617e603beaeba94094641620e9d
David McDonald <dave@Davids-MacBook-Pro.local>
2019-03-05 13:34:41 +1100

    added reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+// Array containing each stored card as an object
 cards = [
 	{
 		rank: "queen",
@@ -22,13 +23,43 @@ cards = [
 ];
 
 
+
+// Shuffle function (Work in progress)
+let shuffleCards = function() {
+	console.log('shuffling cards');
+}
+
+
+// Score counter
+let score = 0;
+let scoreIncrease = function() {
+	score += 1;
+	// console.log('Score is:' + score);
+	document.getElementById('scoreCount').innerHTML = "Score = " + score;
+}
+
+
+// Create a new board by looping through and setting attributes
+let createBoard = function() {
+	for (i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', './images/back.png');
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+// Empty array where cards will be pushed to
 cardsInPlay = [];
 
 
-let checkForMatch = function () {
+let checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert('You found a match!');
+			scoreIncrease();
 		}
 		else {
 			alert('Sorry, try again.');
@@ -37,30 +68,38 @@ let checkForMatch = function () {
 }
 
 
-let flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+// Need to change to only push unique card clicks
+let flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	
+
 	cardsInPlay.push(cards[cardId].rank);
+	
+
+	console.log('User flipped: ' + cards[cardId].rank);
+	console.log('Img: ' + cards[cardId].cardImage);
+	console.log('Suit: ' + cards[cardId].suit);
+	console.log('Id: ' + cardId);
+	console.log(cardsInPlay);
 	checkForMatch();
 }
 
-
-flipCard(0);
-flipCard(2);
-
-/*
-	let cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log('User flipped: ' + cardOne);
-
-	let cardTwo = cards[1];
-	cardsInPlay.push(cardTwo);
-	console.log('User flipped: ' + cardTwo);
+createBoard();
 
 
-*/
+
+let resetBoard = function() {
+	for (i=0; i < cards.length; i++) {
+		document.getElementsByTagName('img')[i].setAttribute('src', 'images/back.png');
+	};
+	cardsInPlay = []; //empty out the array of cards in play
+}
+
+
+// Reset button
+var button = document.getElementById('resetButton');
+button.addEventListener('click', resetBoard);
 
 
 
